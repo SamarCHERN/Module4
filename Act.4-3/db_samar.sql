6e1cd8cb263468f34bab8597fa56fecc42092014
Samar Cherni <scherni@dom.tti>
2022-07-01 18:20:31 +0100

    update on database

--- Act.4-3/db_samar.sql
+++ Act.4-3/db_samar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 01 juil. 2022 à 15:57
+-- Généré le : ven. 01 juil. 2022 à 19:19
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 7.4.29
 
@@ -61,7 +61,29 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20220701100946', '2022-07-01 12:10:08', 1302);
+('DoctrineMigrations\\Version20220701100946', '2022-07-01 12:10:08', 1302),
+('DoctrineMigrations\\Version20220701141032', '2022-07-01 16:10:43', 1985),
+('DoctrineMigrations\\Version20220701152047', '2022-07-01 17:21:14', 2048);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `texte` longtext COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `texte`) VALUES
+(1, 'Votre colis est en cours de préparation'),
+(2, 'Votre colis est expédiée'),
+(3, 'Votre colis est arrivé');
 
 -- --------------------------------------------------------
 
@@ -89,6 +111,48 @@ INSERT INTO `product` (`id`, `category_id`, `name`, `price`, `description`) VALU
 (5, 5, 'Livre Php', 19.99, 'Ergonomic and stylish!'),
 (6, 3, 'Iphone', 19.99, 'Ergonomic and stylish!');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `name`) VALUES
+(1, 'Eric'),
+(2, 'Jhon');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_messages`
+--
+
+CREATE TABLE `user_messages` (
+  `user_id` int(11) NOT NULL,
+  `messages_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user_messages`
+--
+
+INSERT INTO `user_messages` (`user_id`, `messages_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(2, 3);
+
 --
 -- Index pour les tables déchargées
 --
@@ -106,11 +170,31 @@ ALTER TABLE `doctrine_migration_versions`
   ADD PRIMARY KEY (`version`);
 
 --
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_D34A04AD12469DE2` (`category_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_messages`
+--
+ALTER TABLE `user_messages`
+  ADD PRIMARY KEY (`user_id`,`messages_id`),
+  ADD KEY `IDX_3B8FFA96A76ED395` (`user_id`),
+  ADD KEY `IDX_3B8FFA96A5905F5A` (`messages_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -123,10 +207,22 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
@@ -137,6 +233,13 @@ ALTER TABLE `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `FK_D34A04AD12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Contraintes pour la table `user_messages`
+--
+ALTER TABLE `user_messages`
+  ADD CONSTRAINT `FK_3B8FFA96A5905F5A` FOREIGN KEY (`messages_id`) REFERENCES `messages` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_3B8FFA96A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
